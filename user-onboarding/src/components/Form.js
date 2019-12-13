c8abdebe83df62ee985fa154b43eec31ee0336d4
Michael Padiernos <mike.padiernos@gmail.com>
2019-12-12 23:19:46 -0500

    WORK: - declared usestate for users - coded display of form

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,29 +3,55 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const UserForm = () => {
+const UserForm = ({ values, errors, touched, status }) => {
+
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		status && setUsers(users => [...users, status]);
+	}, [status]);
 
 	return (
 		<div className="form form-user">
-			<form>
+			<Form>
 				<label htmlFor="name">Name:
 					<Field
 						type="text"
 						id="name"
+						name="name"
+						placeholder=""
 					/>
+					{touched.name && errors.name && (
+						<p className="errors">{errors.name}</p>
+					)}
 				</label><br />
 				<label htmlFor="email">E-mail:
 					<Field
 						type="text"
 						id="email"
+						name="email"
+						placeholder=""
 					/>
+					{touched.email && errors.email && (
+						<p className="errors">{errors.email}</p>
+					)}
 				</label><br />
 				<label htmlFor="password">Password:
 					<Field
-						type="text"
+						type="password"
 						id="password"
+						name="password"
 					/>
+					{touched.password && errors.password && (
+						<p className="errors">{errors.password}</p>
+					)}
 				</label><br />
+				<Field
+					as="textarea"
+					type="text"
+					name="notes"
+					placeholder="Notes"
+				/><br />
 				<label className="checkbox-container">
 					Terms of Service
 					<Field
@@ -35,9 +61,37 @@ const UserForm = () => {
 					<span className="check-mark" />
 				</label><br />
 				<button type="submit">Submit</button>
-			</form>
+			</Form>
+			{users.map(user => {
+				return (
+					<article key={user.id}>
+						<h2>Name: {user.species}</h2>
+						<p>E-mail: {user.size}</p>
+					</article>
+				)}
+			)}
 		</div>
 	)
 };
 
-export default UserForm;
+const FormikUserForm = withFormik({
+
+	mapPropsToValues(props) {
+		return {
+			name: props.name || "",
+			email: props.email || "",
+			password: props.password || "",
+			notes: props.notes || "",
+			tos: props.tos || false
+		};
+	},
+
+	validationSchema: Yup.object().shape({
+		password: Yup.string().required(),
+		tos: Yup.string().required("You must agree to the Terms of Service!")
+	}),
+
+
+}) (UserForm);
+
+export default FormikUserForm;
