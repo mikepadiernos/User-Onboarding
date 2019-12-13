8046cf5d5836032f370ee9f5528f66447cda7784
Michael Padiernos <mike.padiernos@gmail.com>
2019-12-12 22:45:21 -0500

    WORK: - started skeleton of form using formik

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,3 +2,42 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+
+const UserForm = () => {
+
+	return (
+		<div className="form form-user">
+			<form>
+				<label htmlFor="name">Name:
+					<Field
+						type="text"
+						id="name"
+					/>
+				</label><br />
+				<label htmlFor="email">E-mail:
+					<Field
+						type="text"
+						id="email"
+					/>
+				</label><br />
+				<label htmlFor="password">Password:
+					<Field
+						type="text"
+						id="password"
+					/>
+				</label><br />
+				<label className="checkbox-container">
+					Terms of Service
+					<Field
+						type="checkbox"
+						name="tos"
+						/>
+					<span className="check-mark" />
+				</label><br />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	)
+};
+
+export default UserForm;
